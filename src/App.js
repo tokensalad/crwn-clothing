eff037b9ebbad931d483886523701de9063dca96
tokensalad <gary@quantipy.com>
2020-11-24 11:04:23 +0000

    updated the men items to navigate withRouter Higher Order Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-class App extends Component {
-  render() {
-    return <HomePage></HomePage>
-  }
+const HatsPage = () => (
+  <div>
+    <h1>
+      HATS PAGE
+    </h1>
+  </div>
+)
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/hats" component={HatsPage}></Route>
+      </Switch>
+    </div>
+  )
 }
+
+// class App extends Component {
+//   render() {
+//     return <HomePage></HomePage>
+//   }
+// }
 
 export default App;
